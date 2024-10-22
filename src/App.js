a9c93454f17483c2b3ce044a9cb4e06d44ac0a6a
Aditya <adityakumar8340203184@gmail.com>
2024-10-22 21:17:32 +0530

    added script

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './index.css';
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://chatapp-ophm.onrender.com', {
     withCredentials: true
 });
 
